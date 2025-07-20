25d5d49776817ca0e9224d2c0e7a4e02e571b3d4
SR6792 <sooraj.2050in@gmail.com>
2025-07-20 10:01:35 +0000

    able to fetch weather

--- javas.js
+++ javas.js
@@ -1,19 +1,33 @@
-const apiKey="9fc5b411e57749f8655fd3f782423157";
-const url=`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}`;
+const apiKey="0267664ed658e5f239229a9fb6031be7";
 const input1=document.querySelector(".inp");
+const btn=document.querySelector(".btn");
+const locname=document.querySelector(".name");
+locname.style.cssText="display:flex;align-items:center;";
+function hover(){
+    btn.style.backgroundColor="grey";
+    btn.style.transform="scale(1.3)";
+}
+function unhover(){
+    btn.style.backgroundColor="white";
+    btn.style.transform="scale(1)";
+}
 async function fetchData() {
     try{
         const loc=input1.value.toLowerCase();
-        const response=await fetch(url);
+        const response=await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${loc}&appid=${apiKey}`);
         if(!response.ok){
             throw new Error("Place entered was incorrect");
         }
-        const data=response.json();
-        
+        const data=await response.json();//await to finish the promise
+        console.log(data);
+        const locationN=data.city.name.charAt(0).toUpperCase()+data.city.name.slice(1).toLowerCase();//only first letter caps
+        locname.innerHTML=`<h1>Location:${locationN}</h1>`;
     }
-    catch(event){
-        return event;
+    catch(error){
+        console.error(error);
     }
 }  
-btn=document.querySelector(".btn");
 btn.addEventListener("click",fetchData);
+btn.addEventListener("mouseenter",hover);
+btn.addEventListener("mouseleave",unhover);
+
